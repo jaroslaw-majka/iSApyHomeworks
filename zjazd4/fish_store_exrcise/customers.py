0e752aef550fbc3fa563f84cc04b7c83eb1307db
Jarek Majka <vessekheal@gmail.com>
2021-09-20 13:57:28 +0200

    Skeleton idea for loyalty card func added

--- zjazd4/fish_store_exrcise/customers.py
+++ zjazd4/fish_store_exrcise/customers.py
@@ -6,3 +6,10 @@ class LoyaltyCard:
         self.collected_points = None
         self.customer_phone = phone_no
         LoyaltyCard.next_card_idx += 1
+
+    #TODO Add points incrementation when a sale is made (1 item = 1 point)
+        # Need to create a function in store.py and make it possible to add a card idx when a purchase is made.
+        # Leaving field empty will result with no loyalty card operations (no points added, no history created)
+
+    #TODO History:
+        # History should save the date, name of the item and quantity bought. As well as after purchase points status.
