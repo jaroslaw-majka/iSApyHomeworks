656061e6d846e176695324b9215814000fed0248
Jarek Majka <vessekheal@gmail.com>
2021-10-13 16:45:30 +0200

    Homework 4 solved with extra exercise

--- zjazd6/Howework 4/human.py
+++ zjazd6/Howework 4/human.py
@@ -1,6 +1,16 @@
+from re import search
+
+
 class Human:
     def __init__(self, first_name: str, last_name: str, date_of_birth: str):
         self.first_name = first_name
         self.last_name = last_name
-        self.date_of_birth = date_of_birth
+        self.date_of_birth = self.birth_date_validator(date_of_birth)
         self._age = 1
+
+    def birth_date_validator(self, date_of_birth):
+        date_format_pattern = r'[0-9][0-9]/[0-9][0-9]/[0-9][0-9][0-9][0-9]'
+        if search(date_format_pattern, date_of_birth):
+            return date_of_birth
+        else:
+            raise Exception('Invalid date format. Please use dd/mm/yyyy')
--- zjazd6/Howework 4/user.py
+++ zjazd6/Howework 4/user.py
@@ -1,17 +1,19 @@
+import datetime
+
 from human import Human
 
 
 class User(Human):
     def __init__(self, first_name: str, last_name: str, date_of_birth: str):
         super().__init__(first_name, last_name, date_of_birth)
+        self.set_age(date_of_birth)
 
-    def get_age(self):
+    def get_age(self) -> int:
+        """Protected variable _age getter"""
         return self._age
 
-
-if __name__ == '__main__':
-    uzytkownik = User('Jarek', 'Majka', '01/01/2000')
-    print(uzytkownik.first_name)
-    print(uzytkownik.last_name)
-    print(uzytkownik.date_of_birth)
-    print(uzytkownik.get_age())
+    def set_age(self, date_of_birth) -> None:
+        """Age setter based on provided date of birth"""
+        year_of_birth = int(date_of_birth.split('/')[2])
+        current_year = datetime.datetime.now().year
+        self._age = current_year - year_of_birth
