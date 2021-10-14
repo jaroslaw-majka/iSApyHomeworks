89ccf0c3cefb6be02f00769774a28e4fb4beb220
Jarek Majka <vessekheal@gmail.com>
2021-10-14 20:50:29 +0200

    Code reformatted

--- zjazd6/Howework 4/human.py
+++ zjazd6/Howework 4/human.py
@@ -5,10 +5,10 @@ class Human:
     def __init__(self, first_name: str, last_name: str, date_of_birth: str):
         self.first_name = first_name
         self.last_name = last_name
-        self.date_of_birth = self.birth_date_validator(date_of_birth)
-        self._age = 1
+        self.date_of_birth = self.set_birth_date(date_of_birth)
 
-    def birth_date_validator(self, date_of_birth):
+    def set_birth_date(self, date_of_birth) -> str:
+        """Checks provided birthday format"""
         date_format_pattern = r'[0-9][0-9]/[0-9][0-9]/[0-9][0-9][0-9][0-9]'
         if search(date_format_pattern, date_of_birth):
             return date_of_birth
--- zjazd6/Howework 4/user.py
+++ zjazd6/Howework 4/user.py
@@ -6,14 +6,14 @@ from human import Human
 class User(Human):
     def __init__(self, first_name: str, last_name: str, date_of_birth: str):
         super().__init__(first_name, last_name, date_of_birth)
-        self.set_age(date_of_birth)
+        self._age = self.set_age(date_of_birth)
+
+    def set_age(self, date_of_birth) -> int:
+        """Age setter based on provided date of birth"""
+        year_of_birth = int(date_of_birth.split('/')[2])
+        current_year = datetime.datetime.now().year
+        return current_year - year_of_birth
 
     def get_age(self) -> int:
         """Protected variable _age getter"""
         return self._age
-
-    def set_age(self, date_of_birth) -> None:
-        """Age setter based on provided date of birth"""
-        year_of_birth = int(date_of_birth.split('/')[2])
-        current_year = datetime.datetime.now().year
-        self._age = current_year - year_of_birth
