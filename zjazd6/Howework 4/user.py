17f2a9ee8242dfb09d2827e2323711c914126d1e
Jarek Majka <vessekheal@gmail.com>
2021-10-17 21:57:17 +0200

    @propoerty decorator added

--- zjazd6/Howework 4/user.py
+++ zjazd6/Howework 4/user.py
@@ -6,14 +6,12 @@ from human import Human
 class User(Human):
     def __init__(self, first_name: str, last_name: str, date_of_birth: str):
         super().__init__(first_name, last_name, date_of_birth)
-        self._age = self.set_age(date_of_birth)
+        self._age = None
 
-    def set_age(self, date_of_birth) -> int:
-        """Age setter based on provided date of birth"""
-        year_of_birth = int(date_of_birth.split('/')[2])
+    @property
+    def age(self) -> int:
+        if self._age:
+            return self._age
+        year_of_birth = int(self.date_of_birth.split('/')[2])
         current_year = datetime.datetime.now().year
         return current_year - year_of_birth
-
-    def get_age(self) -> int:
-        """Protected variable _age getter"""
-        return self._age
