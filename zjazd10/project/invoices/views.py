58ba5fd94d2e440e0cca7d95c41089c88eaa8cb0
Jarek Majka <vessekheal@gmail.com>
2021-12-01 12:40:56 +0100

    Basic empty view created and connected

--- zjazd10/project/invoices/views.py
+++ zjazd10/project/invoices/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from invoices.models import Invoice
 
-# Create your views here.
+
+class InvoiceListView(ListView):
+    model = Invoice
+    template = 'invoice_list.html'
--- zjazd10/project/project/urls.py
+++ zjazd10/project/project/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from invoices.views import InvoiceListView
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('invoices/', InvoiceListView.as_view()),
 ]
