83d7739637ea32267ee812565e201c189e87bb66
Jarek Majka <vessekheal@gmail.com>
2021-12-02 22:19:18 +0100

    Total ammount to be paid added to view

--- zjazd10/project/invoices/views.py
+++ zjazd10/project/invoices/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.views.generic import ListView
+from django.db.models import Sum
 
 from invoices.models import Invoice
 
@@ -10,4 +11,5 @@ class InvoiceListView(ListView):
 
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data()
+        context['total_ammount'] = Invoice.objects.aggregate(price_sum=Sum('service__price'))['price_sum']
         return context
