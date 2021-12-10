2e9312ff4031073ed083ba0a8e25ee40949f7b1c
Jarek Majka <vessekheal@gmail.com>
2021-12-10 11:40:40 +0100

    tests written

--- homework9/tests/test_rectangular_triangle.py
+++ homework9/tests/test_rectangular_triangle.py
@@ -1,8 +1,25 @@
+import pytest
 from rectangular_triangle.rectangular_triangle import rectangular_triangle_calculator
 
 
 def test_rectangular_triangle_calculator_input_type():
     value_a = 'a'
     value_b = 'b'
+    with pytest.raises(ValueError):
+        rectangular_triangle_calculator(value_a, value_b)
+
+
+def test_rectangular_triangle_calculator_correct_calculation():
+    value_a = 3
+    value_b = 4
+    expected_result = 5
     result = rectangular_triangle_calculator(value_a, value_b)
-    assert result == ValueError
+    assert result == expected_result
+
+
+def test_rectangular_triangle_calculator_correct_calculation_high_numbers():
+    value_a = 600
+    value_b = 800
+    expected_result = 1000
+    result = rectangular_triangle_calculator(value_a, value_b)
+    assert result == expected_result
