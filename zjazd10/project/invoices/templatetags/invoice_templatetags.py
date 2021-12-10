68daf36fe3ee33b95251c86b49f5e4fda660aa45
Jarek Majka <vessekheal@gmail.com>
2021-12-10 10:23:04 +0100

    Paid invoice underliner added

--- zjazd10/project/invoices/templatetags/invoice_templatetags.py
+++ zjazd10/project/invoices/templatetags/invoice_templatetags.py
@@ -5,7 +5,11 @@ register = template.Library()
 
 
 @register.filter
-def row_styler(payment_date):
+def row_styler(invoice):
+    payment_status = invoice.payment_status
+    payment_date = invoice.payment_date
     today = date.today()
-    if (payment_date - today).days > 7:
+    if payment_status:
+        return 'paid'
+    elif (payment_date - today).days > 7:
         return 'due'
