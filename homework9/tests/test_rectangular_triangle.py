a51abc9ac2da6d9e6c3590721720ffa579c6de08
Jarek Majka <vessekheal@gmail.com>
2021-12-11 13:42:36 +0100

    variable renamed

--- homework9/tests/test_rectangular_triangle.py
+++ homework9/tests/test_rectangular_triangle.py
@@ -12,14 +12,14 @@ def test_rectangular_triangle_calculator_input_type():
 def test_rectangular_triangle_calculator_correct_calculation():
     value_a = 3
     value_b = 4
-    expected_result = 5
+    expected = 5
     result = rectangular_triangle_calculator(value_a, value_b)
-    assert result == expected_result
+    assert result == expected
 
 
 def test_rectangular_triangle_calculator_correct_calculation_high_numbers():
     value_a = 600
     value_b = 800
-    expected_result = 1000
+    expected = 1000
     result = rectangular_triangle_calculator(value_a, value_b)
-    assert result == expected_result
+    assert result == expected
